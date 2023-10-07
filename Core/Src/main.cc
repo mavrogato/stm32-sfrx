867c7f11b274ba62d8fbf916781136fa6a35f1df
Nakashima, Terumi <nakashima.terumi@gmail.com>
2023-10-07 17:21:55 +0900

    WIP.

--- Core/Src/main.cc
+++ Core/Src/main.cc
@@ -1,4 +1,9 @@
+#include <limits>
 #include <bit>
+#include <string_view>
+#include <type_traits>
+#include <array>
+
 #include <cstdio>
 
 #pragma GCC diagnostic push
@@ -8,28 +13,162 @@
 
 extern "C" int __io_putchar(int ch) { return ITM_SendChar(ch); }
 
+namespace aux
+{
+    inline auto get(auto volatile* ptr, auto mask) noexcept {
+        return (*ptr & mask);
+    }
+    inline void reset(auto volatile* ptr, auto val = 0) noexcept {
+        *ptr = val;
+    }
+    inline void set(auto volatile* ptr, auto val) noexcept {
+        *ptr = *ptr | val;
+    }
+    inline void set(auto volatile* ptr, auto val, auto clear) noexcept {
+        *ptr = (*ptr & ~clear) | val;
+    }
+
+    template <class INTERFACE, size_t BASE,
+              class REGISTER, size_t INDEX, size_t OFFSET,
+              REGISTER... MASK>
+    class sfrx {
+    public:
+        using interface_type = INTERFACE;
+        using register_type = REGISTER;
+
+    public:
+        static constexpr auto address = BASE + OFFSET + INDEX * sizeof (register_type);
+        static constexpr auto digits = std::numeric_limits<register_type>::digits;
+        static constexpr auto nof_masks = sizeof... (MASK);
+        static constexpr auto combined_mask = (MASK | ...);
+        static constexpr auto combined_clear = ((std::has_single_bit(MASK) ? 0 : MASK) | ...);
+
+    private:
+        static inline auto pointer() noexcept {
+            return reinterpret_cast<register_type volatile *const>(address);
+        }
+
+        template <REGISTER... VALUE>
+        static void check_values() noexcept {
+            static_assert(sizeof... (MASK) == sizeof... (VALUE));
+            static_assert(0 == ((VALUE & ~(MASK >> std::countr_zero(MASK))) | ...));
+        }
+
+    public:
+        static auto load() noexcept {
+            return std::array<REGISTER, nof_masks>{
+                (get(pointer(), MASK) >> std::countr_zero(MASK))...
+            };
+        }
+
+        template <REGISTER... VALUE>
+        static void confirm() noexcept {
+            check_values<VALUE...>();
+            constexpr REGISTER combined_absolute_value = ((VALUE << std::countr_zero(MASK)) | ...);
+            while (get(pointer(), combined_mask) != combined_absolute_value) continue;
+        }
+
+        template <REGISTER... VALUE>
+        static void reset() noexcept {
+            check_values<VALUE...>();
+            constexpr REGISTER combined_absolute_value = ((VALUE << std::countr_zero(MASK)) | ...);
+            set(pointer(), combined_absolute_value);
+        }
+
+        template <REGISTER... VALUE>
+        static void store() noexcept {
+            check_values<VALUE...>();
+            constexpr REGISTER combined_absolute_value = ((VALUE << std::countr_zero(MASK)) | ...);
+            if constexpr (combined_clear) {
+                set(pointer(), combined_absolute_value, combined_clear);
+            }
+            else {
+                set(pointer(), combined_absolute_value);
+            }
+        }
+        template <REGISTER... VALUE>
+        static void store_confirm() noexcept {
+            store<VALUE...>();
+            confirm<VALUE...>();
+        }
+        template <REGISTER... VALUE>
+        static void store_delay() noexcept {
+            check_values<VALUE...>();
+            ([]() {
+                constexpr REGISTER absolute_value = VALUE << std::countr_zero(MASK);
+                if constexpr (1 < std::popcount(MASK)) {
+                    set(pointer(), absolute_value, MASK);
+                }
+                else {
+                    set(pointer(), absolute_value);
+                }
+                auto tmp = get(pointer(), MASK);
+                (void) tmp;
+            }(), ...);
+        }
+
+    public:
+        static auto debug_string() noexcept {
+            return __PRETTY_FUNCTION__;
+        }
+
+    private:
+        static_assert(std::is_unsigned_v<REGISTER>);
+        static_assert(0 < sizeof...(MASK));
+        static_assert(((digits == std::countl_zero(MASK) + std::popcount(MASK) + std::countr_zero(MASK)) && ...));
+        static_assert(std::popcount((MASK | ...)) == (std::popcount(MASK) + ...));
+    };
+} // ::aux
+
+#define SFRX_IMPL_REC_9(BIN,L,R,...) BIN(L,R)__VA_OPT__(,SFRX_IMPL_REC_9(BIN,L,__VA_ARGS__))
+#define SFRX_IMPL_REC_8(BIN,L,R,...) BIN(L,R)__VA_OPT__(,SFRX_IMPL_REC_9(BIN,L,__VA_ARGS__))
+#define SFRX_IMPL_REC_7(BIN,L,R,...) BIN(L,R)__VA_OPT__(,SFRX_IMPL_REC_8(BIN,L,__VA_ARGS__))
+#define SFRX_IMPL_REC_6(BIN,L,R,...) BIN(L,R)__VA_OPT__(,SFRX_IMPL_REC_7(BIN,L,__VA_ARGS__))
+#define SFRX_IMPL_REC_5(BIN,L,R,...) BIN(L,R)__VA_OPT__(,SFRX_IMPL_REC_6(BIN,L,__VA_ARGS__))
+#define SFRX_IMPL_REC_4(BIN,L,R,...) BIN(L,R)__VA_OPT__(,SFRX_IMPL_REC_5(BIN,L,__VA_ARGS__))
+#define SFRX_IMPL_REC_3(BIN,L,R,...) BIN(L,R)__VA_OPT__(,SFRX_IMPL_REC_4(BIN,L,__VA_ARGS__))
+#define SFRX_IMPL_REC_2(BIN,L,R,...) BIN(L,R)__VA_OPT__(,SFRX_IMPL_REC_3(BIN,L,__VA_ARGS__))
+#define SFRX_IMPL_REC_1(BIN,L,R,...) BIN(L,R)__VA_OPT__(,SFRX_IMPL_REC_2(BIN,L,__VA_ARGS__))
+#define SFRX_IMPL_REC_0(BIN,L,R,...) BIN(L,R)__VA_OPT__(,SFRX_IMPL_REC_1(BIN,L,__VA_ARGS__))
+#define SFRX_IMPL_APPLY(BIN,L,R,...) BIN(L,R)__VA_OPT__(,SFRX_IMPL_REC_0(BIN,L,__VA_ARGS__))
+#define SFRX_IMPL_CONCAT_MSK(SFR,M) SFR##_##M##_Msk
+#define SFRX(I,R,...)                                                    \
+    aux::sfrx<std::remove_pointer_t<decltype (I)>,                       \
+              std::string_view("FLASH") == #I ? FLASH_R_BASE : I##_BASE, \
+              std::remove_cv_t<decltype (I->R)>,                         \
+              0,                                                         \
+              offsetof(std::remove_pointer_t<decltype (I)>, R),          \
+              SFRX_IMPL_APPLY(SFRX_IMPL_CONCAT_MSK, I##_##R, __VA_ARGS__)>
+#define SFxRX(I,x,R,...)                                                    \
+    aux::sfrx<std::remove_pointer_t<decltype (I##x)>,                       \
+              std::string_view("FLASH") == #I ? FLASH_R_BASE : I##x##_BASE, \
+              std::remove_cv_t<decltype (I##x->R)>,                         \
+              0,                                                            \
+              offsetof(std::remove_pointer_t<decltype (I##x)>, R),          \
+              SFRX_IMPL_APPLY(SFRX_IMPL_CONCAT_MSK, I##_##R, __VA_ARGS__)>
+#define SFRnX(I,R,n,...)                                                     \
+    aux::sfrx<std::remove_pointer_t<decltype (I)>,                           \
+              std::string_view("FLASH") == #I ? FLASH_R_BASE : I##_BASE,     \
+              std::remove_reference_t<std::remove_cv_t<decltype (I->R[0])>>, \
+              (n-1),                                                         \
+              offsetof(std::remove_pointer_t<decltype (I)>, R), \
+              SFRX_IMPL_APPLY(SFRX_IMPL_CONCAT_MSK, I##_##R##n, __VA_ARGS__)>
+
 void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
 static void MX_USART2_UART_Init(void);
 
 int main(void)
 {
-  /* USER CODE BEGIN 1 */
-
-  /* USER CODE END 1 */
-
-  /* MCU Configuration--------------------------------------------------------*/
-
-  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
-  LL_APB2_GRP1_EnableClock(LL_APB2_GRP1_PERIPH_SYSCFG);
-  LL_APB1_GRP1_EnableClock(LL_APB1_GRP1_PERIPH_PWR);
+    SFRX(RCC, APB2ENR, SYSCFGEN)::store_delay<1>();
+    SFRX(RCC, APB1ENR, PWREN)::store_delay<1>();
+//  /* MCU Configuration--------------------------------------------------------*/
+//  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+//  LL_APB2_GRP1_EnableClock(LL_APB2_GRP1_PERIPH_SYSCFG);
+//  LL_APB1_GRP1_EnableClock(LL_APB1_GRP1_PERIPH_PWR);
 
   /* System interrupt init*/
   NVIC_SetPriorityGrouping(NVIC_PRIORITYGROUP_0);
-
-  /* USER CODE BEGIN Init */
-
-  /* USER CODE END Init */
 
   /* Configure the system clock */
   SystemClock_Config();
@@ -51,7 +190,7 @@ int main(void)
   {
       LL_mDelay(125);
       LL_GPIO_TogglePin(LD2_GPIO_Port, LD2_Pin);
-      std::puts("Tick...");
+      //std::puts("Tick...");
   }
   /* USER CODE END 3 */
 }
@@ -62,43 +201,59 @@ int main(void)
   */
 void SystemClock_Config(void)
 {
-  LL_FLASH_SetLatency(LL_FLASH_LATENCY_2);
-  while(LL_FLASH_GetLatency()!= LL_FLASH_LATENCY_2)
-  {
-  }
-  LL_PWR_SetRegulVoltageScaling(LL_PWR_REGU_VOLTAGE_SCALE1);
-  LL_RCC_HSI_SetCalibTrimming(16);
-  LL_RCC_HSI_Enable();
+    SFRX(FLASH, ACR, LATENCY)::store_confirm<2>();
+//  LL_FLASH_SetLatency(LL_FLASH_LATENCY_2);
+//  while(LL_FLASH_GetLatency()!= LL_FLASH_LATENCY_2)
+//  {
+//  }
+    SFRX(PWR, CR, VOS)::store<3>();
+//  LL_PWR_SetRegulVoltageScaling(LL_PWR_REGU_VOLTAGE_SCALE1);
+    SFRX(RCC, CR, HSITRIM)::store<16>();
+//  LL_RCC_HSI_SetCalibTrimming(16);
+    SFRX(RCC, CR, HSION)::store<1>();
+//  LL_RCC_HSI_Enable();
 
-   /* Wait till HSI is ready */
-  while(LL_RCC_HSI_IsReady() != 1)
-  {
+    SFRX(RCC, CR, HSIRDY)::confirm<1>();
+//   /* Wait till HSI is ready */
+//  while(LL_RCC_HSI_IsReady() != 1)
+//  {
+//
+//  }
+    SFRX(RCC, PLLCFGR, PLLSRC, PLLM, PLLN, PLLP)::store<0, 16, 336, 1>();
+//  LL_RCC_PLL_ConfigDomain_SYS(LL_RCC_PLLSOURCE_HSI, LL_RCC_PLLM_DIV_16, 336, LL_RCC_PLLP_DIV_4);
+    SFRX(RCC, CR, PLLON)::store_confirm<1>();
+//  LL_RCC_PLL_Enable();
+//   /* Wait till PLL is ready */
+//  while(LL_RCC_PLL_IsReady() != 1)
+//  {
+//
+//  }
+    SFRX(PWR, CSR, VOSRDY)::confirm<1>();
+//  while (LL_PWR_IsActiveFlag_VOS() == 0)
+//  {
+//  }
+    SFRX(RCC, CFGR, HPRE, PPRE1, PPRE2, SW)::store<0, 4, 0, 2>();
+//  LL_RCC_SetAHBPrescaler(LL_RCC_SYSCLK_DIV_1);
+//  LL_RCC_SetAPB1Prescaler(LL_RCC_APB1_DIV_2);
+//  LL_RCC_SetAPB2Prescaler(LL_RCC_APB2_DIV_1);
+//  LL_RCC_SetSysClkSource(LL_RCC_SYS_CLKSOURCE_PLL);
 
-  }
-  LL_RCC_PLL_ConfigDomain_SYS(LL_RCC_PLLSOURCE_HSI, LL_RCC_PLLM_DIV_16, 336, LL_RCC_PLLP_DIV_4);
-  LL_RCC_PLL_Enable();
-
-   /* Wait till PLL is ready */
-  while(LL_RCC_PLL_IsReady() != 1)
-  {
-
-  }
-  while (LL_PWR_IsActiveFlag_VOS() == 0)
-  {
-  }
-  LL_RCC_SetAHBPrescaler(LL_RCC_SYSCLK_DIV_1);
-  LL_RCC_SetAPB1Prescaler(LL_RCC_APB1_DIV_2);
-  LL_RCC_SetAPB2Prescaler(LL_RCC_APB2_DIV_1);
-  LL_RCC_SetSysClkSource(LL_RCC_SYS_CLKSOURCE_PLL);
-
-   /* Wait till System clock is ready */
-  while(LL_RCC_GetSysClkSource() != LL_RCC_SYS_CLKSOURCE_STATUS_PLL)
-  {
-
-  }
-  LL_Init1msTick(84000000);
-  LL_SetSystemCoreClock(84000000);
-  LL_RCC_SetTIMPrescaler(LL_RCC_TIM_PRESCALER_TWICE);
+    SFRX(RCC, CFGR, SWS)::confirm<2>();
+//   /* Wait till System clock is ready */
+//  while(LL_RCC_GetSysClkSource() != LL_RCC_SYS_CLKSOURCE_STATUS_PLL)
+//  {
+//
+//  }
+    constexpr uint32_t HCLK_FREQ = 84'000'000;
+    constexpr uint32_t TICKS_RES = 1'000;
+    SysTick->LOAD = HCLK_FREQ / TICKS_RES - 1;
+    SysTick->VAL = 0;
+    SFRX(SysTick, CTRL, CLKSOURCE, ENABLE)::reset<1, 1>();
+//  LL_Init1msTick(84000000);
+    SystemCoreClock = HCLK_FREQ;
+//  LL_SetSystemCoreClock(84000000);
+    SFRX(RCC, DCKCFGR, TIMPRE)::store<0>();
+//  LL_RCC_SetTIMPrescaler(LL_RCC_TIM_PRESCALER_TWICE);
 }
 
 /**
@@ -159,20 +314,20 @@ static void MX_USART2_UART_Init(void)
   */
 static void MX_GPIO_Init(void)
 {
-/* USER CODE BEGIN MX_GPIO_Init_1 */
-/* USER CODE END MX_GPIO_Init_1 */
+    SFRX(RCC, AHB1ENR, GPIOCEN, GPIOHEN, GPIOAEN, GPIOBEN)::store_delay<1,1,1,1>();
+//  /* GPIO Ports Clock Enable */
+//  LL_AHB1_GRP1_EnableClock(LL_AHB1_GRP1_PERIPH_GPIOC);
+//  LL_AHB1_GRP1_EnableClock(LL_AHB1_GRP1_PERIPH_GPIOH);
+//  LL_AHB1_GRP1_EnableClock(LL_AHB1_GRP1_PERIPH_GPIOA);
+//  LL_AHB1_GRP1_EnableClock(LL_AHB1_GRP1_PERIPH_GPIOB);
 
-  /* GPIO Ports Clock Enable */
-  LL_AHB1_GRP1_EnableClock(LL_AHB1_GRP1_PERIPH_GPIOC);
-  LL_AHB1_GRP1_EnableClock(LL_AHB1_GRP1_PERIPH_GPIOH);
-  LL_AHB1_GRP1_EnableClock(LL_AHB1_GRP1_PERIPH_GPIOA);
-  LL_AHB1_GRP1_EnableClock(LL_AHB1_GRP1_PERIPH_GPIOB);
+    SFxRX(GPIO, A, BSRR, BR5)::store<1>();
+//  /**/
+//  LL_GPIO_ResetOutputPin(LD2_GPIO_Port, LD2_Pin);
 
-  /**/
-  LL_GPIO_ResetOutputPin(LD2_GPIO_Port, LD2_Pin);
-
-  /**/
-  LL_SYSCFG_SetEXTISource(LL_SYSCFG_EXTI_PORTC, LL_SYSCFG_EXTI_LINE13);
+    SFRnX(SYSCFG, EXTICR, 4, EXTI13)::store<2>();
+//  /**/
+//  LL_SYSCFG_SetEXTISource(LL_SYSCFG_EXTI_PORTC, LL_SYSCFG_EXTI_LINE13);
 
   /**/
   LL_EXTI_InitTypeDef EXTI_InitStruct = {};
